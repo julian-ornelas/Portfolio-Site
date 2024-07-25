4a84bbff8af615e6672b7e062b6a377e46f4bde2
Julian Ornelas <julian_ornelas45@protonmail.com>
2024-07-24 21:02:01 -0400

    Added personal experience, need to add or remove urls

--- src/i18n/en/en_experience.ts
+++ src/i18n/en/en_experience.ts
@@ -3,37 +3,50 @@ import type { Experience } from '@/types'
 const experience: Experience = {
   title: 'Experience',
   description: 'You can download my resume by clicking the button below.',
-  download_button: 'Download CV',
+  download_button: 'Download resume',
   present: 'Present',
   month: 'month | months',
   year: 'year | years',
   list: [
     {
-      company: 'Apple Inc.',
-      position: 'Chief Executive Officer',
+      company: 'CodePath',
+      position: 'Cybersecurity Tech Fellow',
       url: 'http://www.applebutwithmorebananas.com',
-      startDate: '2011-03',
-      endDate: 'Present',
+      startDate: '2023-07',
+      endDate: '2023-11',
       highlights: [
-        'Invented the iFruit series, which includes the revolutionary Apple Banana: the first smartphone you can also eat.',
-        'Successfully negotiated peace treaties with rival tech giants using the persuasive power of interpretative dance.',
-        'Launched the Macbook Airless: the first laptop that actually floats in the air.'
+        'Mentored over 20 students through technical labs and projects, with topics including OSINT, cryptography, and encryption.',
+        'Achieved a 95% satisfactory response rate to student questions and concerns by delivering prompt guidance and assistance on Slack.'
       ],
       description:
-        "Since taking the helm at Apple Inc., I've steered the company through waves of innovation while wearing a superhero cape in all executive meetings. My leadership style focuses on unleashing creativity, fostering an environment where software engineers are encouraged to wear pajamas to boost productivity, and replacing all office chairs with bean bags for ergonomic reasons."
+        "In an online Zoom class regarding cybersecurity, I had to help students through their breakout room problems, verifying that they understood the concepts and helping with any questions they had. I was friendly and helpful to my students, and overall made sure they enjoyed the class."
     },
     {
-      company: 'Google LLC',
-      position: 'Global Head of Easter Eggs',
+      company: 'PwC x Paragon One',
+      position: 'Nonprofit Consulting Extern',
       url: 'http://www.googolplexthegame.com',
-      startDate: '2015-04',
-      endDate: 'Present',
+      startDate: '2023-06',
+      endDate: '2023-08',
       highlights: [
-        'Created the world’s first AI-powered Easter egg that only appears when users correctly guess the number of jelly beans in a virtual jar',
-        'Organized an international hide-and-seek tournament using Google Earth, which increased user engagement by 5000%.'
+        'Synthesized presentation for 2 selected donors highlighting giving trends, potential funding opportunities, and strategic recommendations.',
+        'Secured up to $20,000 in grants by evaluating the potential impact of funding from selected organizations.'
       ],
       description:
-        'As the Global Head of Easter Eggs at Google LLC, I have championed the infusion of fun and surprises into our products. My work ensures that every search and click unveils a potential smile, enhancing user interaction and deepening brand loyalty. I have also been pivotal in the development of the ‘Snack Search’ feature, where the Google AI suggests snacks based on users’ browsing history, significantly improving afternoon productivity.'
+        'I was tasked with helping a STEM nonprofit gain funding for their activities. I had to analyze financial capabilities and make sure philanthropic organizations were a match with the nonprofit. Then I presented my findings and pitched suggestions on why the nonprofit board should attempt to secure funding from those organizations.'
+    }
+    ,
+    {
+      company: 'Fulton County Schools',
+      position: 'Information Technology Intern',
+      url: 'http://www.googolplexthegame.com',
+      startDate: '2022-06',
+      endDate: '2022-07',
+      highlights: [
+        'Applied troubleshooting methodology on over 500 laptops to assess for physical and software issues.',
+        'Created and updated Azure AD profiles for 60% of laptops using hash and model number, tagged laptops appropriately.'
+      ],
+      description:
+        'I was able to assist in the re-imaging and preparing of school laptops for the next school year. It was a numbers game of trying to get as many laptops ready for the students while still ensuring quality performance, while communicating with my supervisors about how the laptops are coming along.'
     }
   ]
 }
